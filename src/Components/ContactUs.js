0794b9e408b545ece909bfa7a970cc1aef45ac6a
Khiyanat-Deori <deorikhiyanat@gmail.com>
2024-09-01 10:39:13 +0530

    feat: sticky contact footer

--- src/Components/ContactUs.js
+++ src/Components/ContactUs.js
@@ -8,25 +8,27 @@ const Contact = () => {
   return (
     <>
       <Navbar />
-      <div className="contact-container">
-        <div className="contact-item">
-          <FaMapMarkerAlt size={24} className="contact-icon" />
-          <h2>Address</h2>
-          <p>Moidhali Pathar, Domal Road, Morigaon, Assam. 782105</p>
+      <div className="wrapper">
+        <div className="contact-container">
+          <div className="contact-item">
+            <FaMapMarkerAlt size={24} className="contact-icon" />
+            <h2>Address</h2>
+            <p>Moidhali Pathar, Domal Road, Morigaon, Assam. 782105</p>
+          </div>
+          <div className="contact-item">
+            <FaPhoneAlt size={24} className="contact-icon" />
+            <h2>Call Us</h2>
+            <p><a href="tel:+918822686715">+91-8822686715</a></p>
+            <p><a href="tel:+919435201000">+91-9435201000</a></p>
+          </div>
+          <div className="contact-item">
+            <FaEnvelope size={24} className="contact-icon" />
+            <h2>Email Us</h2>
+            <p><a href="mailto:sultanhospital2017@gmail.com">sultanhospital2017@gmail.com</a></p>
+          </div>
         </div>
-        <div className="contact-item">
-          <FaPhoneAlt size={24} className="contact-icon" />
-          <h2>Call Us</h2>
-          <p><a href="tel:+918822686715">+91-8822686715</a></p>
-          <p><a href="tel:+919435201000">+91-9435201000</a></p>
-        </div>
-        <div className="contact-item">
-          <FaEnvelope size={24} className="contact-icon" />
-          <h2>Email Us</h2>
-          <p><a href="mailto:sultanhospital2017@gmail.com">sultanhospital2017@gmail.com</a></p>
-        </div>
+        <Footer />
       </div>
-      <Footer />
     </>
   );
 }
