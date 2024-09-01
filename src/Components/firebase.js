c297744c712921f87be9e8fde3753186643824fc
Khiyanat-Deori <deorikhiyanat@gmail.com>
2024-09-01 11:11:33 +0530

    feat: security

--- src/Components/firebase.js
+++ src/Components/firebase.js
@@ -3,18 +3,18 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyASSrPzgTivU7YrSaFKBF-3fq8LAF3mbHo",
-  authDomain: "sultanwebapp-573f2.firebaseapp.com",
-  projectId: "sultanwebapp-573f2",
-  storageBucket: "sultanwebapp-573f2.appspot.com",
-  messagingSenderId: "479381336316",
-  appId: "1:479381336316:web:c407c105c5c1e336386779",
-  measurementId: "G-23RD90FM6Z"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
-
 const db = getFirestore(app);
 const auth = getAuth(app);
 
 export { db, auth };
+
